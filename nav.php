ad2b841bd711f1c6be9b5c660bc2a71cbb786218
jenniferdixon129 <dixonjennifer129@gmail.com>
2016-08-11 11:28:18 -0700

    initialpush

---
+++ nav.php
@@ -0,0 +1,41 @@
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+                    <li>
+                        <a href="digital.php">Digital</a>
+                    </li>
+                    <li>
+                        <a href="print.php">Print</a>
+                    </li>
+                     <li>
+                        <a href="finearts.php">Fine Arts</a>
+                        <ul>
+                        <li><a href="drawingandpainting.php">Drawing & Painting</a></li>
+                        <li><a href="photography.php">Photography</a></li>
+                        <li><a href="3d.php">3D</a></li>
+                        </ul>
+                    </li>
+                     <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
